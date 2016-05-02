8974c598f1400d406be1e9d4ce06fc247fcd8bc3
ikotermin <ikotermin@hotmail.com>
2016-05-02 13:19:58 +0200

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,14 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+library(ggplot2)
+Balt<- subset(NEI,fips=="24510")
+png("plot3.png")
+ggplot(Balt, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
+  geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
+  ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') + 
+  ggtitle('Emissions per Type in Baltimore City, Maryland') +
+  geom_jitter(alpha=0.10)
+
+dev.off()
